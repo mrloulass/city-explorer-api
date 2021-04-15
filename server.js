6d18dbd15f70707bad08a689377ea1084b33481e
mrloulass <louislassegue@gmail.com>
2021-04-14 21:40:06 -0700

    data server updated

--- server.js
+++ server.js
@@ -1,43 +1,45 @@
-// console.log('SUP');
+'use strict';
 
 // need each line of code for every node server build
-const express = require('express');
 
-const cors = require('cors');
+const express = require('express'); //  express for express server
 
 require('dotenv').config(); // this allow you to use dotenv file
 
-const weatherData = require('./data/weather.json'); // import the json file
+const cors = require('cors');
 
 const app = express();
-
 
 app.use(cors());// allow REACT app forntend to access data form server
 
 const PORT = process.env.PORT || 3001;
 
-function Forecast(date, description) {
-  this.date = date;
-  this.description = description;
+const weatherData = require('./data/weather.json'); // import the json file
+
+
+function Forecast(day) {
+  this.description = day.weather.description;
+  this.date = day.datetime;
+}
+
+function handleErrors(error, response) {
+  response.status(500).send('internal error');
 }
 
 // server definition goes next
 // servers are set up to listen at some path for particular method(get,post,put)
 // listening for GET request at the path /
 
-app.get('/', (request, response) => {
-  // get the request, send a response
-  response.send('hello');
+app.get('/weather', (request, response) => {
+  try {
+    const allForecastData = weatherData.data.map(day => new Forecast(day));
+    response.send(allForecastData);
+    // response.json(allForecastData);
+    console.log(allForecastData);
+  } catch (error) {
+    handleErrors(error, response);
+  }
 });
 
-app.get('/weather', (request, response) => {
-  let locationData = `lat: ${weatherData.lat} lon: ${weatherData.lon}`;
-  response.json(locationData);
-});
-
-app.get('/weather', (request, response) => {
-let newArray = Forecast.map(weatherData);
-  response.json(newArray);
-});
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
